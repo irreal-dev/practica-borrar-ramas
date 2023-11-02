d5b327cb54b8fed28abec452c83d1dad617c4b3c
irreal-dev <miguel.moreno.vl@gmail.com>
2023-11-02 12:44:54 +0100

    cambios antes de borrado

--- file3.py
+++ file3.py
@@ -1,1 +1,3 @@
-print('tercer archivo')
+print('tercer archivo, en este archivo estamos poniendo mucha informacion')
+console.log('ganamos estrellas aqui la hemos cagado por que estamos metiendo Js')
+No queremos que nadie se entere y no podemos hacer un commit publico por lo tanto hacemos un stash
